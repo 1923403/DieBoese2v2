40947a14391e571418682db1b13d0e143343d6de
Steffen <Steffen@192.168.24.208>
2020-07-27 16:57:28 +0200

    changes in Move

--- DieBoese2v2/src/model/Move.java
+++ DieBoese2v2/src/model/Move.java
@@ -1,10 +1,30 @@
 package model;
 
+import java.awt.Point;
+
 public class Move {
 
 	private Board board;
 
 	public Move(Board board) {
 		this.board = board;
+	}
+
+	// checks if there is an empty space on the board
+	public boolean movePossible() {
+		var array = this.board.getBoard();
+		for (var x = 0; x < array.length; x++) {
+			for (var y = 0; y < array.length; y++) {
+				if (array[x][y] != ' ')
+					return false;
+			}
+		}
+		return true;
+	}
+
+	// places given figure at given coordinates on the board
+	public void setMove(Point coordinates, char figure) {
+		var array = this.board.getBoard();
+		array[coordinates.x][coordinates.y] = figure;
 	}
 }
