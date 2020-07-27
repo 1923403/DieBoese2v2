af41638744a3ac7d023293652531c083931e9d10
Steffen <Steffen@192.168.24.208>
2020-07-27 19:14:05 +0200

    removed isFull and setMove from Board
    implemented secondMove, block, and copyBoard in Move

--- DieBoese2v2/src/model/Move.java
+++ DieBoese2v2/src/model/Move.java
@@ -10,6 +10,11 @@ public class Move {
 		this.board = board.getBoard();
 	}
 
+	public boolean hasWon() {
+		// under construction
+		return false;
+	}
+
 	// checks if there is an empty space on the board
 	public boolean movePossible() {
 		for (var x = 0; x < this.board.length; x++) {
@@ -29,7 +34,7 @@ public class Move {
 		if (turnCount < 7) {
 			this.block(coordinates);
 		} else if (turnCount == 9) {
-			this.secondMove(coordinates);
+			this.secondMove(coordinates, figure);
 		} else {
 			this.setMove(coordinates, figure);
 		}
@@ -37,24 +42,79 @@ public class Move {
 		this.capture(coordinates);
 	}
 
-	private void block(Point coordinates) {
+	// DEFAULT FOR TESTING
+	void block(Point coordinates) {
+		this.setMove(coordinates, 'B');
 	}
 
-	private void capture(Point coordinates) {
-
+	// DEFAULT FOR TESTING
+	void capture(Point coordinates) {
+		// under construction
 	}
 
+	// DEFAULT FOR TESTING
+	// makes a copy of the original board
+	char[][] copyBoard() {
+		var copy = new char[this.board.length][this.board.length];
+
+		for (var x = 0; x < copy.length; x++) {
+			for (var y = 0; y < copy.length; y++) {
+				var tmp = this.board[x][y];
+				copy[x][y] = tmp;
+			}
+		}
+
+		return copy;
+	}
+
+	// DEFAULT FOR TESTING
 	// checks if space is empty
-	private boolean isValidMove(Point coordinates) {
+	boolean isValidMove(Point coordinates) {
 		return this.board[coordinates.x][coordinates.y] == ' ';
 	}
 
-	private void secondMove(Point coordinates) {
+	// DEFAULT FOR TESTING
+	// second regular move of first player where several fields are blocked
+	void secondMove(Point coordinates, char figure) throws InvalidMoveException {
+		var tmpArray = this.copyBoard();
+		var block = 'B';
 
+		// up
+		tmpArray[0][tmpArray.length / 2] = block;
+		// down
+		tmpArray[tmpArray.length - 1][tmpArray.length / 2] = block;
+		// left
+		tmpArray[tmpArray.length / 2][0] = block;
+		// right
+		tmpArray[tmpArray.length / 2][tmpArray.length - 1] = block;
+
+		if ((tmpArray.length % 2) == 0) {
+			// up
+			tmpArray[0][(tmpArray.length / 2) + 1] = block;
+			// down
+			tmpArray[tmpArray.length - 1][(tmpArray.length / 2) + 1] = block;
+			// left
+			tmpArray[(tmpArray.length / 2) + 1][0] = block;
+			// right
+			tmpArray[(tmpArray.length / 2) + 1][tmpArray.length - 1] = block;
+		}
+
+		for (var x = 1; x < (tmpArray.length - 1); x++) {
+			for (var y = 1; y < (tmpArray.length - 1); y++) {
+				tmpArray[x][y] = block;
+			}
+		}
+
+		if (tmpArray[coordinates.x][coordinates.y] == ' ') {
+			this.setMove(coordinates, figure);
+		} else {
+			throw new InvalidMoveException();
+		}
 	}
 
+	// DEFAULT FOR TESTING
 	// access through setMove(Point, char, int), block(Point), and secondMove(Point)
-	private void setMove(Point coordinates, char figure) {
+	void setMove(Point coordinates, char figure) {
 		this.board[coordinates.x][coordinates.y] = figure;
 	}
 }
