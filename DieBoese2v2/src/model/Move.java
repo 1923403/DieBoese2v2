7eb2e74fc0c07e62c1ee3970a1dff57fb5aacf90
Steffen <Steffen@192.168.24.208>
2020-07-27 14:32:42 +0200

    added a reference of Board to Move
    edited class diagram

--- DieBoese2v2/src/model/Move.java
+++ DieBoese2v2/src/model/Move.java
@@ -2,8 +2,13 @@ package model;
 
 public class Move {
 
+	private Board board;
 	private int[] enemyMove;
 	private int[] myMove;
+
+	public Move(Board board) {
+		this.board = board;
+	}
 
 	private void setEnemyMove(final int[] move) {
 		this.enemyMove = move;
