05c841890decf9dcebb120a42f942544b6cb3e47
mahehsma <56866320+mahehsma@users.noreply.github.com>
2020-07-27 17:52:29 +0200

    fixed printBoard

--- DieBoese2v2/src/model/Board.java
+++ DieBoese2v2/src/model/Board.java
@@ -18,12 +18,12 @@ public class Board {
 		for (var x = 0; x < board.length; x++) {
 			for (var y = 0; y < board.length; y++) {
 				if (board[x][y] != ' ') // at least one empty field left
-					return false; 
+					return false;
 			}
 		}
 		return true;
 	}
-	
+
 	/**
 	 * 
 	 * @return current board
@@ -31,33 +31,38 @@ public class Board {
 	public char[][] getBoard() {
 		return this.board;
 	}
-	
+
 	/**
 	 * prints board, adds chars for rows, adds numbers for column
 	 */
 	public void printBoard() {
-		for (int y = 0; y <= board.length+1; y++) {
+		for (int y = 0; y <= board.length + 1; y++) {
 			System.out.println();
-			if(y != 0 && y != board.length + 1) System.out.print((char)(97+board.length-(y))); // chars on the left
-			for (int x = 0; x < board.length; x++) {
-				if(y == 0 || y== board.length +1) { 
-					if(x == 0) System.out.print(" ");
-					if(x < 10) System.out.print(" ");
-					System.out.print((x+1)+" ");
-				}
-				else System.out.print("["+board[x][y-1] + "]");// numbers
+			if (y != 0 && y != board.length + 1) {
+				if (y > 6)
+					System.out.print(" ");
+				System.out.print((board.length - y + 1)); // numbers on the left
 			}
-			if(y != 0 && y != board.length + 1) System.out.print((char)(97+board.length-(y)));// chars on the right
+			for (int x = 0; x < board.length; x++) {
+				if (y == 0 || y == board.length + 1) {
+					if (x == 0)
+						System.out.print(" ");
+					System.out.print("  " + (char) (97 + x));// chars in row
+				} else
+					System.out.print("[" + board[x][y - 1] + "]");
+			}
+			if (y != 0 && y != board.length + 1)
+				System.out.print(board.length - y + 1);// numbers on the right
 		}
 		System.out.println("\n"); // empty line after every board
 	}
 
-	
-	//for testing only
+	// for testing only
 	public static void main(String[] args) {
-		new Board(10).printBoard();
-		System.out.println();
-		new Board(15).printBoard();
+
+		var testBoard = new Board(15);
+		testBoard.board[0][0] = 'X';
+		testBoard.printBoard();
 
 	}
 }
