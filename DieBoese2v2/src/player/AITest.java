732162b0fc986b00198548e96f8a734da65af702
Steffen <Steffen@192.168.24.208>
2020-07-27 10:57:24 +0200

    up

---
+++ DieBoese2v2/src/player/AITest.java
@@ -0,0 +1,14 @@
+package player;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AITest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
