3f4cff5ded86444189844557ba3394700c0b9cd3
Steffen <Steffen@192.168.24.208>
2020-07-27 14:13:48 +0200

    added move, convertCoordinates, and isValidString to Player
    edited class diagram

--- DieBoese2v2/src/model/Move.java
+++ DieBoese2v2/src/model/Move.java
@@ -5,22 +5,6 @@ public class Move {
 	private int[] enemyMove;
 	private int[] myMove;
 
-	// numbers represent y-coordinates, letters x-coordinates
-	private int[] convertCoordinates(final String coordinates) {
-		final var array = new int[2];
-		var number = "";
-
-		for (var i = 0; i < coordinates.length(); i++)
-			if ((coordinates.charAt(i) < 58))
-				number += coordinates.charAt(i);
-			else
-				array[1] = coordinates.toUpperCase().charAt(i);
-
-		array[0] = Integer.valueOf(number);
-
-		return array;
-	}
-
 	private void setEnemyMove(final int[] move) {
 		this.enemyMove = move;
 	}
