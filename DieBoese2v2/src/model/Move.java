62ec0a21a43f0f4085bd746903671e6489fab8b6
Steffen <Steffen@192.168.24.208>
2020-07-28 09:58:03 +0200

    updated class diagram
    edited setMove, isValidMove, and secondMove in Move
    
    added needed transfer parameters to capture method

--- DieBoese2v2/src/model/Move.java
+++ DieBoese2v2/src/model/Move.java
@@ -4,81 +4,80 @@ import java.awt.Point;
 
 public class Move {
 
-	private char[][] board;
+	private final char[][] board;
 
-	public Move(Board board) {
+	public Move(final Board board) {
 		this.board = board.getBoard();
 	}
 
 	public boolean hasWon() {
 		// under construction
-		//if(board.isFull()) return true;
+		// if(!movePossible()) return true;
 		return false;
 	}
 
 	// checks if there is an empty space on the board
 	public boolean movePossible() {
-		for (var x = 0; x < this.board.length; x++) {
-			for (var y = 0; y < this.board.length; y++) {
-				if (this.board[x][y] != ' ')
+		for (final char[] element : this.board)
+			for (var y = 0; y < this.board.length; y++)
+				if (element[y] != ' ')
 					return false;
-			}
-		}
 		return true;
 	}
 
 	// places given figure at given coordinates on the board
-	public void setMove(Point coordinates, char figure, int turnCount) throws InvalidMoveException {
-		if (!this.isValidMove(coordinates))
-			throw new InvalidMoveException("Field is not empty!");
+	public void setMove(final Point coordinates, final char figure, final char enemyFigure, final int turnCount)
+			throws InvalidMoveException {
+		this.isValidMove(this.board, coordinates);
 
-		if (turnCount < 7) {
+		if (turnCount < 7)
 			this.block(coordinates);
-		} else if (turnCount == 9) {
+		else if (turnCount == 9)
 			this.secondMove(coordinates, figure);
-		} else {
+		else
 			this.setMove(coordinates, figure);
-		}
 
-		this.capture(coordinates);
+		this.capture(coordinates, figure, enemyFigure);
 	}
 
 	// DEFAULT FOR TESTING
-	void block(Point coordinates) {
+	void block(final Point coordinates) {
 		this.setMove(coordinates, 'B');
 	}
 
 	// DEFAULT FOR TESTING
-	void capture(Point coordinates) {
+	void capture(final Point coordinates, final char figure, final char enemyFigure) {
 		// under construction
 	}
 
 	// DEFAULT FOR TESTING
 	// makes a copy of the original board
 	char[][] copyBoard() {
-		var copy = new char[this.board.length][this.board.length];
+		final var copy = new char[this.board.length][this.board.length];
 
-		for (var x = 0; x < copy.length; x++) {
+		for (var x = 0; x < copy.length; x++)
 			for (var y = 0; y < copy.length; y++) {
-				var tmp = this.board[x][y];
+				final var tmp = this.board[x][y];
 				copy[x][y] = tmp;
 			}
-		}
 
 		return copy;
 	}
 
 	// DEFAULT FOR TESTING
 	// checks if space is empty
-	boolean isValidMove(Point coordinates) {
-		return this.board[coordinates.x][coordinates.y] == ' ';
+	boolean isValidMove(final char[][] board, final Point coordinates) throws InvalidMoveException {
+		if (this.board[coordinates.x][coordinates.y] == ' ')
+			return true;
+		else
+			throw new InvalidMoveException("Field is not empty!");
 	}
 
 	// DEFAULT FOR TESTING
 	// second regular move of first player where several fields are blocked
-	void secondMove(Point coordinates, char figure) throws InvalidMoveException {
-		var tmpArray = this.copyBoard();
-		var block = 'B';
+	void secondMove(final Point coordinates, final char figure) throws InvalidMoveException {
+		final var tmpArray = this.copyBoard();
+		final var block = 'B';
 
 		// up
 		tmpArray[0][tmpArray.length / 2] = block;
@@ -100,22 +99,17 @@ public class Move {
 			tmpArray[(tmpArray.length / 2) + 1][tmpArray.length - 1] = block;
 		}
 
-		for (var x = 1; x < (tmpArray.length - 1); x++) {
-			for (var y = 1; y < (tmpArray.length - 1); y++) {
+		for (var x = 1; x < (tmpArray.length - 1); x++)
+			for (var y = 1; y < (tmpArray.length - 1); y++)
 				tmpArray[x][y] = block;
-			}
-		}
 
-		if (tmpArray[coordinates.x][coordinates.y] == ' ') {
+		if (this.isValidMove(tmpArray, coordinates))
 			this.setMove(coordinates, figure);
-		} else {
-			throw new InvalidMoveException();
-		}
 	}
 
 	// DEFAULT FOR TESTING
 	// access through setMove(Point, char, int), block(Point), and secondMove(Point)
-	void setMove(Point coordinates, char figure) {
+	void setMove(final Point coordinates, final char figure) {
 		this.board[coordinates.x][coordinates.y] = figure;
 	}
 }
