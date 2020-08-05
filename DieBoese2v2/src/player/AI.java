4b2d29884cd280e0a6c3f1d9f4320bafb08afb01
Steffen <Steffen@192.168.24.208>
2020-08-05 21:28:49 +0200

    saves its next turn in myMove, extends from Player

--- DieBoese2v2/src/player/AI.java
+++ DieBoese2v2/src/player/AI.java
@@ -2,10 +2,6 @@ package player;
 
 import java.awt.Point;
 
-import model.Board;
-import model.InvalidMoveException;
-import model.Move;
-
 public class AI extends Player {
 
 	private Point enemyMove;
@@ -14,29 +10,23 @@ public class AI extends Player {
 		super(figure);
 	}
 
+	@Override
+	public void move(final int boardSize, char enemyFigure, int turnCount) {
+		// creates random move
+		System.out.println("AI starts move... boardSize: " + boardSize);
+		Point aiMove = new Point();
+		aiMove.x = (int) ((Math.random() * (boardSize)));
+		aiMove.y = (int) ((Math.random() * (boardSize)));
+		System.out.println("AI: " + aiMove.toString());
+		this.setMyMove(aiMove);
+	}
+
 	public void setEnemyMove(final Point move) {
 		this.enemyMove = move;
 	}
 
-	@Override
-	public void move(final int boardSize, Move move, char enemyFigure, int turnCount) {
-		// creates random move
-		System.out.println("AI starts move... boardSize: "+ boardSize);
-		Point aiMove = new Point();
-		aiMove.x = (int) ((Math.random() * (boardSize)));
-		aiMove.y = (int) ((Math.random() * (boardSize)));
-		System.out.println("AI: "+aiMove.toString());
-		try {
-			move.setMove(aiMove, this.getFigure(), enemyFigure, turnCount);
-			myMove = aiMove;
-		} catch (InvalidMoveException e) {
-			move(boardSize, move, enemyFigure, turnCount);
-		}
-	}
-	
-	
-	public static void main(String[] args) {
-		var testAI = new AI('O');
-		testAI.move(15, new Move(new Board(15)), 'X', 0);
-	}
+//	public static void main(String[] args) {
+//		var testAI = new AI('O');
+//		testAI.move(15, new Move(new Board(15)), 'X', 0);
+//	}
 }
