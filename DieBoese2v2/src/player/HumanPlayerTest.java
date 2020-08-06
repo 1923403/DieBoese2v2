91d7b73ca4cac4e079b72630c5ad5cc3e0386ffe
Steffen <Steffen@192.168.24.208>
2020-08-06 11:17:59 +0200

    finished tests for isValidString

--- DieBoese2v2/src/player/HumanPlayerTest.java
+++ DieBoese2v2/src/player/HumanPlayerTest.java
@@ -3,53 +3,105 @@ package player;
 import java.awt.Point;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class HumanPlayerTest {
 
+	private static HumanPlayer player = new HumanPlayer('X');
+	private static Point point = new Point(3, 1);
+
+	@DisplayName("cc: leading number, small letter")
 	@Test
 	void testConvertCoordinate1() {
-		var player = new HumanPlayer('X');
-		var point = new Point(2, 0);
-		Assert.assertEquals(player.convertCoordinates(3, "3c"), point);
+		Assert.assertEquals(HumanPlayerTest.player.convertCoordinates(4, "3d"), HumanPlayerTest.point);
 	}
 
+	@DisplayName("cc: leading small letter, number")
 	@Test
 	void testConvertCoordinate2() {
-		var player = new HumanPlayer('X');
-		var point = new Point(2, 0);
-		Assert.assertEquals(player.convertCoordinates(3, "c3"), point);
+		Assert.assertEquals(HumanPlayerTest.player.convertCoordinates(4, "d3"), HumanPlayerTest.point);
 	}
 
+	@DisplayName("cc: leading number, capital letter")
 	@Test
 	void testConvertCoordinate3() {
-		var player = new HumanPlayer('X');
-		var point = new Point(2, 0);
-		Assert.assertEquals(player.convertCoordinates(3, "3C"), point);
+		Assert.assertEquals(HumanPlayerTest.player.convertCoordinates(4, "3D"), HumanPlayerTest.point);
 	}
 
+	@DisplayName("cc: leading capital letter, number")
 	@Test
 	void testConvertCoordinate4() {
-		var player = new HumanPlayer('X');
-		var point = new Point(2, 0);
-		Assert.assertEquals(player.convertCoordinates(3, "C3"), point);
+		Assert.assertEquals(HumanPlayerTest.player.convertCoordinates(4, "D3"), HumanPlayerTest.point);
 	}
 
+	@DisplayName("ivs: string length < 2")
 	@Test
-	void testIsValidStirng() {
-		var player = new HumanPlayer('X');
-		Assert.assertTrue("no valid String", player.isValidString(15, "a1"));
-		Assert.assertTrue(player.isValidString(15, "1a"));
-		Assert.assertTrue(player.isValidString(15, "a10"));
-		Assert.assertTrue(player.isValidString(15, "10a"));
-		Assert.assertFalse(player.isValidString(15, " "));
-		Assert.assertFalse(player.isValidString(15, "1"));
-		Assert.assertFalse(player.isValidString(15, "a"));
-		Assert.assertFalse(player.isValidString(15, "11"));
-		Assert.assertFalse(player.isValidString(15, "ab"));
-		Assert.assertFalse(player.isValidString(15, "1ab"));
-		Assert.assertFalse(player.isValidString(15, "ab1"));
-		Assert.assertFalse(player.isValidString(15, "16a"));
-		Assert.assertFalse(player.isValidString(15, "a16"));
+	void testIsValidString01() {
+		Assert.assertFalse(HumanPlayerTest.player.isValidString(15, "1"));
+	}
+
+	@DisplayName("ivs: string length > 3")
+	@Test
+	void testIsValidString02() {
+		Assert.assertFalse(HumanPlayerTest.player.isValidString(15, "1111"));
+	}
+
+	@DisplayName("ivs: string length == 3, 2nd character is a letter")
+	@Test
+	void testIsValidString03() {
+		Assert.assertFalse(HumanPlayerTest.player.isValidString(15, "1a1"));
+	}
+
+	@DisplayName("ivs: character is neither a letter nor a number")
+	@Test
+	void testIsValidString04() {
+		Assert.assertFalse(HumanPlayerTest.player.isValidString(15, "/1"));
+		Assert.assertFalse(HumanPlayerTest.player.isValidString(15, "1/"));
+		Assert.assertFalse(HumanPlayerTest.player.isValidString(15, ":1"));
+		Assert.assertFalse(HumanPlayerTest.player.isValidString(15, "1:"));
+		Assert.assertFalse(HumanPlayerTest.player.isValidString(15, "`1"));
+		Assert.assertFalse(HumanPlayerTest.player.isValidString(15, "1`"));
+		Assert.assertFalse(HumanPlayerTest.player.isValidString(15, "{1"));
+		Assert.assertFalse(HumanPlayerTest.player.isValidString(15, "1{"));
+	}
+
+	@DisplayName("ivs: letter count > 1")
+	@Test
+	void testIsValidString05() {
+		Assert.assertFalse(HumanPlayerTest.player.isValidString(15, "aa1"));
+		Assert.assertFalse(HumanPlayerTest.player.isValidString(15, "1aa"));
+	}
+
+	@DisplayName("ivs: letter count == 0")
+	@Test
+	void testIsValidString06() {
+		Assert.assertFalse(HumanPlayerTest.player.isValidString(15, "111"));
+	}
+
+	@DisplayName("ivs: valid letter")
+	@Test
+	void testIsValidString07() {
+		Assert.assertTrue(HumanPlayerTest.player.isValidString(15, "o1"));
+	}
+
+	@DisplayName("ivs: invalid letter")
+	@Test
+	void testIsValidString08() {
+		Assert.assertFalse(HumanPlayerTest.player.isValidString(15, "p1"));
+	}
+
+	@DisplayName("ivs: valid number")
+	@Test
+	void testIsValidString09() {
+		Assert.assertTrue(HumanPlayerTest.player.isValidString(15, "a1"));
+		Assert.assertTrue(HumanPlayerTest.player.isValidString(15, "a15"));
+	}
+
+	@DisplayName("ivs: invalid number")
+	@Test
+	void testIsValidString10() {
+		Assert.assertFalse(HumanPlayerTest.player.isValidString(15, "a0"));
+		Assert.assertFalse(HumanPlayerTest.player.isValidString(15, "a16"));
 	}
 }
