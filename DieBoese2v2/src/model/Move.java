a872ffa7c265c62d05a2568b9332b09e6e7c1834
mahehsma <56866320+mahehsma@users.noreply.github.com>
2020-08-09 16:32:21 +0200

    merged

--- DieBoese2v2/src/model/Move.java
+++ DieBoese2v2/src/model/Move.java
@@ -14,74 +14,15 @@ public class Move {
 		// under construction
 //		if (coordinates != null)
 //			System.out.println("longest row: " + longestRow(figure, coordinates));
-		if (coordinates != null && longestRow(figure, coordinates) >= 5) {
+		if ((coordinates != null) && (this.longestRow(figure, coordinates) >= 5)) {
 			return true;
 		}
-		if (!movePossible()) {
+		if (!this.movePossible()) {
 			System.out.println("no more move possible");
 			return true;
 		}
 
 		return false;
-	}
-
-	/**
-	 * 
-	 * @param figure      	figure which the algorithm is looking for
-	 * @param coordinates 	starting point
-	 * @return 				longest row
-	 */
-	private int longestRow(char figure, Point coordinates) {
-		// under construction, could be optimized
-		// horizontal row
-		System.out.println(coordinates);
-		Point direction = new Point();
-		direction.x = 1;
-		direction.y = 0;
-		var horizontalFigures = figuresInRow(figure, coordinates, direction);
-
-		direction.x = 0;
-		direction.y = 1;
-		var verticalFigures = figuresInRow(figure, coordinates, direction);
-
-		direction.x = 1;
-		direction.y = 1;
-		var diagonalFigures1 = figuresInRow(figure, coordinates, direction);
-
-		direction.x = 1;
-		direction.y = -1;
-		var diagonalFigures2 = figuresInRow(figure, coordinates, direction);
-
-		return Math.max(Math.max(horizontalFigures, verticalFigures), Math.max(diagonalFigures1, diagonalFigures2));
-	}
-
-	/**
-	 * counts how many same figures are in the row next to the last placed figure
-	 * 
-	 * @param figure      placed figure / symbol
-	 * @param coordinates position where figure was placed
-	 * @param direction
-	 * @return
-	 */
-	private int figuresInRow(char figure, Point coordinates, Point direction) {
-		var counter = 1;
-		var posX = coordinates.x + direction.x;
-		var posY = coordinates.y + direction.y;
-		var boardLength = board.getBoard().length;
-		while (posX < boardLength && posY < boardLength && posY >= 0 && board.getBoard()[posX][posY] == figure) {
-			counter++;
-			posX += direction.x;
-			posY += direction.y;
-		}
-		posX = coordinates.x - direction.x;
-		posY = coordinates.y - direction.y;
-		while (posX >= 0 && posY < boardLength && posY >= 0 && board.getBoard()[posX][posY] == figure) {
-			counter++;
-			posX -= direction.x;
-			posY -= direction.y;
-		}
-//		System.out.println("direction: " + direction + ", length: " + counter);
-		return counter;
 	}
 
 	// checks if there is an empty space on the board
@@ -94,18 +35,29 @@ public class Move {
 	}
 
 	// places given figure at given coordinates on the board
-	public void setMove(final Point coordinates, final char figure, final char enemyFigure, final int turnCount)
-			throws InvalidMoveException {
-		this.isValidMove(this.board.getBoard(), coordinates);
+	public boolean setMove(final Point coordinates, final char figure, final char enemyFigure, final int turnCount) {
+		try {
+			this.isValidMove(this.board.getBoard(), coordinates);
+		} catch (InvalidMoveException e) {
+			System.err.println(e.getMessage());
+			return false;
+		}
 
 		if (turnCount < 7)
 			this.block(coordinates);
 		else if (turnCount == 9)
-			this.secondMove(coordinates, figure);
+			try {
+				this.isValidMove(this.secondMove(), coordinates);
+			} catch (InvalidMoveException e) {
+				System.err.println(e.getMessage());
+				return false;
+			}
 		else
 			this.setMove(coordinates, figure);
 
 		this.capture(coordinates, figure, enemyFigure);
+
+		return true;
 	}
 
 	// DEFAULT FOR TESTING
@@ -120,78 +72,48 @@ public class Move {
 		// right
 		direction.x = 1;
 		direction.y = 0;
-		captureDirections(coordinates, direction, figure, enemyFigure);
+		this.captureDirections(coordinates, direction, figure, enemyFigure);
 
 		// left
 		direction.x = -1;
 		direction.y = 0;
-		captureDirections(coordinates, direction, figure, enemyFigure);
+		this.captureDirections(coordinates, direction, figure, enemyFigure);
 
 		// top
 		direction.x = 0;
 		direction.y = 1;
-		captureDirections(coordinates, direction, figure, enemyFigure);
+		this.captureDirections(coordinates, direction, figure, enemyFigure);
 
 		// bottom
 		direction.x = 0;
 		direction.y = -1;
-		captureDirections(coordinates, direction, figure, enemyFigure);
+		this.captureDirections(coordinates, direction, figure, enemyFigure);
 
 		// top right
 		direction.x = 1;
 		direction.y = 1;
-		captureDirections(coordinates, direction, figure, enemyFigure);
+		this.captureDirections(coordinates, direction, figure, enemyFigure);
 
 		// bottom right
 		direction.x = 1;
 		direction.y = -1;
-		captureDirections(coordinates, direction, figure, enemyFigure);
+		this.captureDirections(coordinates, direction, figure, enemyFigure);
 
 		// bottom left
 		direction.x = -1;
 		direction.y = -1;
-		captureDirections(coordinates, direction, figure, enemyFigure);
+		this.captureDirections(coordinates, direction, figure, enemyFigure);
 
 		// top left
 		direction.x = -1;
 		direction.y = 1;
-		captureDirections(coordinates, direction, figure, enemyFigure);
-	}
-
-	/**
-	 * direction from capture, checks if capturing is possible 
-	 * in this specific direction
-	 * 
-	 * @param coordinates
-	 * @param direction
-	 * @param figure
-	 * @param enemyFigure
-	 */
-	private void captureDirections(Point coordinates, Point direction, char figure, char enemyFigure) {
-		var point1 = new Point(); //should be enemys figure for capturing
-		var point2 = new Point(); //should be enemys figure for capturing
-		var point3 = new Point(); //should be players figure for capturing
-		point1.x = coordinates.x + direction.x;
-		point1.y = coordinates.y + direction.y;
-		point2.x = point1.x + direction.x;
-		point2.y = point1.y + direction.y;
-		point3.x = point2.x + direction.x;
-		point3.y = point2.y + direction.y;
-		if ((point3.x < board.getBoard().length) && (point3.y < board.getBoard().length) && (point3.x >= 0)
-				&& (point3.y >= 0) && (point3.x >= 0)) {//checks if this point is located on board
-			if (board.getBoard()[point3.x][point3.y] == figure && board.getBoard()[point1.x][point1.y] == enemyFigure
-					&& board.getBoard()[point2.x][point2.y] == enemyFigure) {//checks if p3 == own figure and p1 == p2 == enemy figure
-				System.out.println("captured...");
-				setMove(point1, ' '); //deletes enemy figure
-				setMove(point2, ' ');
-			}
-		}
+		this.captureDirections(coordinates, direction, figure, enemyFigure);
 	}
 
 	// DEFAULT FOR TESTING
 	// checks if space is empty
 	boolean isValidMove(final char[][] board, final Point coordinates) throws InvalidMoveException {
-		if (this.board.getBoard()[coordinates.x][coordinates.y] == ' ')
+		if (board[coordinates.x][coordinates.y] == ' ')
 			return true;
 		else
 			throw new InvalidMoveException("Field is not empty!");
@@ -199,7 +121,7 @@ public class Move {
 
 	// DEFAULT FOR TESTING
 	// second regular move of first player where several fields are blocked
-	void secondMove(final Point coordinates, final char figure) throws InvalidMoveException {
+	char[][] secondMove() {
 		System.out.println("second move..."); // debug
 		final var tmpArray = this.board.copyBoard();
 		final var block = 'B';
@@ -228,8 +150,7 @@ public class Move {
 			for (var y = 1; y < (tmpArray.length - 1); y++)
 				tmpArray[x][y] = block;
 
-		if (this.isValidMove(tmpArray, coordinates))
-			this.setMove(coordinates, figure);
+		return tmpArray;
 	}
 
 	// DEFAULT FOR TESTING
@@ -237,5 +158,100 @@ public class Move {
 	void setMove(final Point coordinates, final char figure) {
 		System.out.println("places figure on: " + coordinates.x + ", " + coordinates.y);
 		this.board.getBoard()[coordinates.x][coordinates.y] = figure;
+	}
+
+	/**
+	 * direction from capture, checks if capturing is possible in this specific
+	 * direction
+	 *
+	 * @param coordinates
+	 * @param direction
+	 * @param figure
+	 * @param enemyFigure
+	 */
+	private void captureDirections(Point coordinates, Point direction, char figure, char enemyFigure) {
+		var point1 = new Point(); // should be enemys figure for capturing
+		var point2 = new Point(); // should be enemys figure for capturing
+		var point3 = new Point(); // should be players figure for capturing
+		point1.x = coordinates.x + direction.x;
+		point1.y = coordinates.y + direction.y;
+		point2.x = point1.x + direction.x;
+		point2.y = point1.y + direction.y;
+		point3.x = point2.x + direction.x;
+		point3.y = point2.y + direction.y;
+		if ((point3.x < this.board.getBoard().length) && (point3.y < this.board.getBoard().length) && (point3.x >= 0)
+				&& (point3.y >= 0) && (point3.x >= 0)) {// checks if this point is located on board
+			if ((this.board.getBoard()[point3.x][point3.y] == figure)
+					&& (this.board.getBoard()[point1.x][point1.y] == enemyFigure)
+					&& (this.board.getBoard()[point2.x][point2.y] == enemyFigure)) {// checks if p3 == own figure and p1
+																					// == p2 == enemy figure
+				System.out.println("captured...");
+				this.setMove(point1, ' '); // deletes enemy figure
+				this.setMove(point2, ' ');
+			}
+		}
+	}
+
+	/**
+	 * counts how many same figures are in the row next to the last placed figure
+	 *
+	 * @param figure      placed figure / symbol
+	 * @param coordinates position where figure was placed
+	 * @param direction
+	 * @return
+	 */
+	private int figuresInRow(char figure, Point coordinates, Point direction) {
+		// not working
+//		if (board.getBoard()[coordinates.x][coordinates.y] != figure)
+//			System.err.println("FEHLER!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		var counter = 1;
+		var posX = coordinates.x + direction.x;
+		var posY = coordinates.y + direction.y;
+		var boardLength = this.board.getBoard().length;
+		while ((posX < boardLength) && (posY < boardLength) && (posY >= 0)
+				&& (this.board.getBoard()[posX][posY] == figure)) {
+			counter++;
+			posX += direction.x;
+			posY += direction.y;
+		}
+		posX = coordinates.x - direction.x;
+		posY = coordinates.y - direction.y;
+		while ((posX >= 0) && (posY < boardLength) && (posY >= 0) && (this.board.getBoard()[posX][posY] == figure)) {
+			counter++;
+			posX -= direction.x;
+			posY -= direction.y;
+		}
+//		System.out.println("direction: " + direction + ", length: " + counter);
+		return counter;
+	}
+
+	/**
+	 *
+	 * @param figure      figure which the algorithm is looking for
+	 * @param coordinates starting point
+	 * @return longest row
+	 */
+	private int longestRow(char figure, Point coordinates) {
+		// under construction, not working yet
+		// horizontal row
+		System.out.println(coordinates);
+		Point direction = new Point();
+		direction.x = 1;
+		direction.y = 0;
+		var horizontalFigures = this.figuresInRow(figure, coordinates, direction);
+
+		direction.x = 0;
+		direction.y = 1;
+		var verticalFigures = this.figuresInRow(figure, coordinates, direction);
+
+		direction.x = 1;
+		direction.y = 1;
+		var diagonalFigures1 = this.figuresInRow(figure, coordinates, direction);
+
+		direction.x = 1;
+		direction.y = -1;
+		var diagonalFigures2 = this.figuresInRow(figure, coordinates, direction);
+
+		return Math.max(Math.max(horizontalFigures, verticalFigures), Math.max(diagonalFigures1, diagonalFigures2));
 	}
 }
