f9ffb528b1731e26e328cc5a736119e72b682bf5
mahehsma <56866320+mahehsma@users.noreply.github.com>
2020-08-10 11:32:11 +0200

    added static printBoard

--- DieBoese2v2/src/model/Board.java
+++ DieBoese2v2/src/model/Board.java
@@ -25,30 +25,34 @@ public class Board {
 	public char[][] getBoard() {
 		return this.board;
 	}
+	
+	public static void printBord(char[][] board) {
+		for (int y = 0; y <= (board.length + 1); y++) {
+			System.out.println();
+			if ((y != 0) && (y != (board.length + 1))) {
+				if ((board.length - y) < 9) // for alignment
+					System.out.print(" ");
+				System.out.print(((board.length - y) + 1)); // numbers on the left
+			}
+			for (int x = 0; x < board.length; x++) {
+				if ((y == 0) || (y == (board.length + 1))) {
+					if (x == 0)
+						System.out.print(" ");
+					System.out.print("  " + (char) (97 + x));// chars in row
+				} else
+					System.out.print("[" + board[x][y - 1] + "]");
+			}
+			if ((y != 0) && (y != (board.length + 1)))
+				System.out.print((board.length - y) + 1);// numbers on the right
+		}
+		System.out.println("\n"); // empty line after every board
+	}
 
 	/**
 	 * prints board, adds chars for rows, adds numbers for column
 	 */
 	public void printBoard() {
-		for (int y = 0; y <= (this.board.length + 1); y++) {
-			System.out.println();
-			if ((y != 0) && (y != (this.board.length + 1))) {
-				if ((this.board.length - y) < 9) // for alignment
-					System.out.print(" ");
-				System.out.print(((this.board.length - y) + 1)); // numbers on the left
-			}
-			for (int x = 0; x < this.board.length; x++) {
-				if ((y == 0) || (y == (this.board.length + 1))) {
-					if (x == 0)
-						System.out.print(" ");
-					System.out.print("  " + (char) (97 + x));// chars in row
-				} else
-					System.out.print("[" + this.board[x][y - 1] + "]");
-			}
-			if ((y != 0) && (y != (this.board.length + 1)))
-				System.out.print((this.board.length - y) + 1);// numbers on the right
-		}
-		System.out.println("\n"); // empty line after every board
+		Board.printBord(this.board);
 	}
 
 	// DEFAULT FOR TESTING
