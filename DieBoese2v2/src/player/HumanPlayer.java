321d2a569aaef86f5d18adc554e9b356d1a3ceac
Steffen <Steffen@192.168.24.208>
2020-08-19 17:09:55 +0200

    added individual error descriptions to HumanPlayer, changed return types from boolean to void, changed method names, updated class diagram

--- DieBoese2v2/src/player/HumanPlayer.java
+++ DieBoese2v2/src/player/HumanPlayer.java
@@ -50,79 +50,97 @@ public class HumanPlayer extends Player {
 	}
 
 	/**
-	 * checks if character is a letter or a number
+	 * checks string and sets move
 	 */
-	private boolean hasValidCharacters(final String coordinates) {
+	private void move(final int boardSize, final String coordinates) throws InvalidStringException {
+		this.validateString(boardSize, coordinates);
+		this.setMyMove(this.convertCoordinates(boardSize, coordinates));
+	}
+
+	/**
+	 * checks if character is a letter or a number
+	 *
+	 * @throws InvalidStringException
+	 */
+	private void validateCharacters(final String coordinates) throws InvalidStringException {
 		for (var i = 0; i < coordinates.length(); i++)
 			if ((coordinates.charAt(i) < 48)
 					|| ((coordinates.charAt(i) > 57) && (coordinates.charAt(i) < 97))
 					|| (coordinates.charAt(i) > 122))
-				return false;
-		return true;
+				throw new InvalidStringException(coordinates.charAt(i) + " is not a valid character!");
 	}
 
 	/**
 	 * checks if string length is 2 or 3
+	 *
+	 * @throws InvalidStringException
 	 */
-	private boolean hasValidLength(final String coordinates) {
-		return ((coordinates.length() >= 2) && (coordinates.length() <= 3));
-	}
-
-	/**
-	 * checks if there is only one letter
-	 */
-	private boolean hasValidLetterCount(final String coordinates) {
-		var letterCount = 0;
-		for (var i = 0; i < coordinates.length(); i++)
-			if (coordinates.charAt(i) > 96)
-				letterCount++;
-		return letterCount == 1;
-	}
-
-	/**
-	 * checks if second character is a letter in case string has a length of 3
-	 */
-	private boolean isInValidOrder(final String coordinates) {
-		if ((coordinates.length() == 3) && (coordinates.charAt(1) > 96))
-			return false;
-		else
-			return true;
+	private void validateLength(final String coordinates) throws InvalidStringException {
+		if (coordinates.length() < 2)
+			throw new InvalidStringException("Please enter more than one character!");
+		if (coordinates.length() > 3)
+			throw new InvalidStringException("Please don't enter more than three characters!");
 	}
 
 	/**
 	 * checks if letter is valid
+	 *
+	 * @throws InvalidStringException
 	 */
-	private boolean isValidLetter(final int boardSize, final String coordinates) {
+	private void validateLetter(final int boardSize, final String coordinates) throws InvalidStringException {
 		for (var i = 0; i < coordinates.length(); i++)
-			if (coordinates.charAt(i) > 96)
+			if (coordinates.charAt(i) > 96) {
 				if ((coordinates.charAt(i) - 97) > (boardSize - 1))
-					return false;
-		return true;
+					throw new InvalidStringException(
+							"Please enter a letter between a and " + (char) (boardSize + 96) + "!");
+			}
+	}
+
+	/**
+	 * checks if there is only one letter
+	 *
+	 * @throws InvalidStringException
+	 */
+	private void validateLetterCount(final String coordinates) throws InvalidStringException {
+		var letterCount = 0;
+		for (var i = 0; i < coordinates.length(); i++) {
+			if (coordinates.charAt(i) > 96) {
+				if (++letterCount > 1)
+					throw new InvalidStringException("Please don't enter more than one letter!");
+			}
+		}
 	}
 
 	/**
 	 * checks if number is valid
+	 *
+	 * @throws InvalidStringException
 	 */
-	private boolean isValidNumber(final int boardSize, final String coordinates) {
+	private void validateNumber(final int boardSize, final String coordinates) throws InvalidStringException {
 		var number = "";
 		for (var i = 0; i < coordinates.length(); i++)
 			if ((coordinates.charAt(i) < 58))
 				number += coordinates.charAt(i);
-		return ((Integer.valueOf(number) > 0) && (Integer.valueOf(number) <= (boardSize)));
-	}
-
-	private void isValidString(final int boardSize, final String coordinates) throws InvalidStringException {
-		if (!this.hasValidLength(coordinates) || !this.hasValidCharacters(coordinates)
-				|| !this.isInValidOrder(coordinates) || !this.isValidLetter(boardSize, coordinates)
-				|| !this.hasValidLetterCount(coordinates) || !this.isValidNumber(boardSize, coordinates))
-			throw new InvalidStringException("You have entered invalid coordinates!");
+		if ((Integer.valueOf(number) <= 0) || (Integer.valueOf(number) > (boardSize)))
+			throw new InvalidStringException("Please enter a number between 1 and " + boardSize + "!");
 	}
 
 	/**
-	 * checks string and sets move
+	 * checks if second character is a letter in case string has a length of 3
+	 *
+	 * @throws InvalidStringException
 	 */
-	private void move(final int boardSize, final String coordinates) throws InvalidStringException {
-		this.isValidString(boardSize, coordinates);
-		this.setMyMove(this.convertCoordinates(boardSize, coordinates));
+	private void validateOrder(final String coordinates) throws InvalidStringException {
+		if ((coordinates.length() == 3) && (coordinates.charAt(1) > 96))
+			throw new InvalidStringException("Please don't put a letter between two numbers!");
+	}
+
+	private void validateString(final int boardSize, final String coordinates) throws InvalidStringException {
+		this.validateLength(coordinates);
+		this.validateCharacters(coordinates);
+		this.validateLetterCount(coordinates);
+		this.validateOrder(coordinates);
+		this.validateLetter(boardSize, coordinates);
+		this.validateNumber(boardSize, coordinates);
 	}
 }
