c4f18ff70ce5744729022f4851e7d527e0d9706d
mahehsma <maxi.hehl@gmail.com>
2020-09-02 11:36:16 +0200

    up

--- DieBoese2v2/src/player/BoardEvaluation.java
+++ DieBoese2v2/src/player/BoardEvaluation.java
@@ -6,42 +6,36 @@ import java.util.HashMap;
 
 public class BoardEvaluation {
 
-	private final char myFigure;
 	private final char enemyFigure;
+	private final char myFigure;
 
 	public BoardEvaluation(char myFigure, char enemyFigure) {
 		this.myFigure = myFigure;
 		this.enemyFigure = enemyFigure;
 	}
 
-	public HashMap<Point, Integer> evaluatePoints(char[][] board, ArrayList<Point> allPoints) {
-		var evaluatedPoints = new HashMap<Point, Integer>();
-		// System.out.println("Vorbewertung:");
-		for (var point : allPoints) {
-			if (winPossible(board, point)) {
-				var evaluation = Math.max(longestRow(board, myFigure, point), longestRow(board, enemyFigure, point));
-				evaluatedPoints.put(point, evaluation);
-				// System.out.println(point + " " + evaluation);
-			}
-		}
-		// System.out.println("---Ende---");
-		return evaluatedPoints;
-	}
-
-	private boolean winPossible(char[][] board, Point move) {
-		return ((longestRow(board, myFigure, ' ', move) > 4) || (longestRow(board, enemyFigure, ' ', move) > 4));
-	}
-
 	public int evaluateBoard(char[][] board, ArrayList<Point> lastMoves, boolean isMaximizing) {
 		var myLongestRow = 0;
 		var longestEnemyRow = 0;
 		for (var i = 0; i < lastMoves.size(); i++) {
-			if (i % 2 == 0) {
-				myLongestRow = Math.max(myLongestRow, longestRow(board, myFigure, lastMoves.get(i)));
+			if ((i % 2) == 0) {
+				if (this.longestRow(board, this.myFigure, ' ', lastMoves.get(i)) >= 5)
+					myLongestRow = Math.max(myLongestRow, this.longestRow(board, this.myFigure, lastMoves.get(i)));
 			} else {
-				longestEnemyRow = Math.max(longestEnemyRow, longestRow(board, enemyFigure, lastMoves.get(i)));
+				if (this.longestRow(board, this.enemyFigure, ' ', lastMoves.get(i)) >= 5)
+					longestEnemyRow = Math.max(longestEnemyRow,
+							this.longestRow(board, this.enemyFigure, lastMoves.get(i)));
 			}
 		}
+		// win possible muss fuer jede richtung ausprobieren und nicht fuer alle
+		// gleichzeitig
+		// gleiches fuer longest row
+
+//		System.out.println("MylongestRow: " + myLongestRow);
+//		System.out.println("longestEnemyRow: " + longestEnemyRow);
+//		if ((myLongestRow >= 3) || (longestEnemyRow >= 3)) {
+//			System.err.println("444444444444444444444444444444");
+//		}
 		if (isMaximizing) {
 			if (myLongestRow >= longestEnemyRow)
 				return myLongestRow;
@@ -55,44 +49,26 @@ public class BoardEvaluation {
 		}
 	}
 
+	public HashMap<Point, Integer> evaluatePoints(char[][] board, ArrayList<Point> allPoints) {
+		var evaluatedPoints = new HashMap<Point, Integer>();
+		// System.out.println("Vorbewertung:");
+		for (var point : allPoints) {
+			if (this.winPossible(board, point)) {
+				var evaluation = Math.max(this.longestRow(board, this.myFigure, point),
+						this.longestRow(board, this.enemyFigure, point));
+				evaluatedPoints.put(point, evaluation);
+				// System.out.println(point + " " + evaluation);
+			}
+		}
+		// System.out.println("---Ende---");
+//		System.out.println(evaluatedPoints);
+		return evaluatedPoints;
+	}
+
 	public boolean hasWon(char[][] board, Point lastMove) {
 		var figure = board[lastMove.x][lastMove.y];
-		return (longestRow(board, figure, lastMove) > 4);
+		return (this.longestRow(board, figure, lastMove) > 4);
 
-	}
-
-	// from class Turn
-
-	/**
-	 *
-	 * @param figure      figure which the algorithm is looking for
-	 * @param coordinates starting point
-	 * @return longest row
-	 */
-	private int longestRow(char[][] board, final char figure, final Point coordinates) {
-		return longestRow(board, figure, figure, coordinates); // row
-	}
-
-	private int longestRow(char[][] board, final char figure, final char blank, final Point coordinates) {
-		// only coordinates required, figure not needed
-
-		// horizontal row
-		final Point horizontal = new Point(1, 0);
-		final var horizontalFigures = this.figuresInRow(board, figure, blank, coordinates, horizontal);
-
-		// vertical row
-		final Point vertical = new Point(0, 1);
-		final var verticalFigures = this.figuresInRow(board, figure, blank, coordinates, vertical);
-
-		// diagonal row (top right to bottom left)
-		final Point diagonal1 = new Point(1, 1);
-		final var diagonalFigures1 = this.figuresInRow(board, figure, blank, coordinates, diagonal1);
-
-		// diagonal row (top left to bottom right)
-		final Point diagonal2 = new Point(1, -1);
-		final var diagonalFigures2 = this.figuresInRow(board, figure, blank, coordinates, diagonal2);
-
-		return Math.max(Math.max(horizontalFigures, verticalFigures), Math.max(diagonalFigures1, diagonalFigures2));
 	}
 
 	/**
@@ -125,5 +101,44 @@ public class BoardEvaluation {
 			posY -= direction.y;
 		}
 		return counter;
+	}
+
+	// from class Turn
+
+	private int longestRow(char[][] board, final char figure, final char blank, final Point coordinates) {
+		// only coordinates required, figure not needed
+
+		// horizontal row
+		final Point horizontal = new Point(1, 0);
+		final var horizontalFigures = this.figuresInRow(board, figure, blank, coordinates, horizontal);
+
+		// vertical row
+		final Point vertical = new Point(0, 1);
+		final var verticalFigures = this.figuresInRow(board, figure, blank, coordinates, vertical);
+
+		// diagonal row (top right to bottom left)
+		final Point diagonal1 = new Point(1, 1);
+		final var diagonalFigures1 = this.figuresInRow(board, figure, blank, coordinates, diagonal1);
+
+		// diagonal row (top left to bottom right)
+		final Point diagonal2 = new Point(1, -1);
+		final var diagonalFigures2 = this.figuresInRow(board, figure, blank, coordinates, diagonal2);
+
+		return Math.max(Math.max(horizontalFigures, verticalFigures), Math.max(diagonalFigures1, diagonalFigures2));
+	}
+
+	/**
+	 *
+	 * @param figure      figure which the algorithm is looking for
+	 * @param coordinates starting point
+	 * @return longest row
+	 */
+	private int longestRow(char[][] board, final char figure, final Point coordinates) {
+		return this.longestRow(board, figure, figure, coordinates); // row
+	}
+
+	private boolean winPossible(char[][] board, Point move) {
+		return ((this.longestRow(board, this.myFigure, ' ', move) > 4)
+				|| (this.longestRow(board, this.enemyFigure, ' ', move) > 4));
 	}
 }
