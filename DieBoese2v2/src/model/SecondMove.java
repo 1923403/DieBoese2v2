a695f6ce727dac6901abe719f4599f1d215e996b
steff <steff@192.168.24.232>
2020-09-12 23:28:30 +0200

    changed method names

--- DieBoese2v2/src/model/SecondMove.java
+++ DieBoese2v2/src/model/SecondMove.java
@@ -9,49 +9,49 @@ public class SecondMove {
 	}
 
 	public char[][] run() {
-		this.secondMoveBlockTop();
-		this.secondMoveBlockBottom();
-		this.secondMoveBlockLeft();
-		this.secondMoveBlockRight();
-		this.secondMoveBlockSquare();
+		this.blockTopRow();
+		this.blockBottomRow();
+		this.blockLeftRow();
+		this.blockRightRow();
+		this.blockSquare();
 		return this.board;
+	}
+
+	private void blockBottomRow() {
+		this.blockField(this.board.length / 2, this.board.length - 1);
+		if (this.hasEvenLength())
+			this.blockField((this.board.length / 2) - 1, this.board.length - 1);
+	}
+
+	private void blockField(final int x, final int y) {
+		this.board[y][x] = 'B';
+	}
+
+	private void blockLeftRow() {
+		this.blockField(0, this.board.length / 2);
+		if (this.hasEvenLength())
+			this.blockField(0, (this.board.length / 2) - 1);
+	}
+
+	private void blockRightRow() {
+		this.blockField(this.board.length - 1, this.board.length / 2);
+		if (this.hasEvenLength())
+			this.blockField(this.board.length - 1, (this.board.length / 2) - 1);
+	}
+
+	private void blockSquare() {
+		for (var x = 1; x < (this.board.length - 1); x++)
+			for (var y = 1; y < (this.board.length - 1); y++)
+				this.blockField(x, y);
+	}
+
+	private void blockTopRow() {
+		this.blockField(this.board.length / 2, 0);
+		if (this.hasEvenLength())
+			this.blockField((this.board.length / 2) - 1, 0);
 	}
 
 	private boolean hasEvenLength() {
 		return (this.board.length % 2) == 0;
-	}
-
-	private void secondMoveBlockBottom() {
-		this.secondMoveBlockFields(this.board.length / 2, this.board.length - 1);
-		if (this.hasEvenLength())
-			this.secondMoveBlockFields((this.board.length / 2) - 1, this.board.length - 1);
-	}
-
-	private void secondMoveBlockFields(final int x, final int y) {
-		this.board[y][x] = 'B';
-	}
-
-	private void secondMoveBlockLeft() {
-		this.secondMoveBlockFields(0, this.board.length / 2);
-		if (this.hasEvenLength())
-			this.secondMoveBlockFields(0, (this.board.length / 2) - 1);
-	}
-
-	private void secondMoveBlockRight() {
-		this.secondMoveBlockFields(this.board.length - 1, this.board.length / 2);
-		if (this.hasEvenLength())
-			this.secondMoveBlockFields(this.board.length - 1, (this.board.length / 2) - 1);
-	}
-
-	private void secondMoveBlockSquare() {
-		for (var x = 1; x < (this.board.length - 1); x++)
-			for (var y = 1; y < (this.board.length - 1); y++)
-				this.secondMoveBlockFields(x, y);
-	}
-
-	private void secondMoveBlockTop() {
-		this.secondMoveBlockFields(this.board.length / 2, 0);
-		if (this.hasEvenLength())
-			this.secondMoveBlockFields((this.board.length / 2) - 1, 0);
 	}
 }
