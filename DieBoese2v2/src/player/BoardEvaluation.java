b79f5daeaa43ca8fea4e51fe5c21fb2c86344ef5
Steffen <Steffen@192.168.24.208>
2020-09-14 15:47:58 +0200

    testing

--- DieBoese2v2/src/player/BoardEvaluation.java
+++ DieBoese2v2/src/player/BoardEvaluation.java
@@ -22,16 +22,76 @@ public class BoardEvaluation {
 		var myLongestRow = 0;
 		var longestEnemyRow = 0;
 		for (var i = 0; i < lastMoves.length; i++) {
-			if ((lastMoves[i].x == 0) && (lastMoves[i].y == 0) && (board[lastMoves[i].x][lastMoves[i].y] == ' '))
-				System.err.println("last moves fehler!!!");
-			if (board[lastMoves[i].x][lastMoves[i].y] != ' ') { // if figure got captured during minimax
-				if ((i % 2) == 0)
-					myLongestRow = Math.max(myLongestRow, this.longestRow(board, this.myFigure, lastMoves[i]));
+//			if ((lastMoves[i].x == 0) && (lastMoves[i].y == 0) && (board[lastMoves[i].x][lastMoves[i].y] == ' '))
+//				System.err.println("last moves fehler!!!");
+			if (!this.fieldIsEmpty(board, lastMoves[i])) { // if figure got captured during minimax
+				if (this.positionIsEven(i))
+					myLongestRow = this.compareLongestRow(myLongestRow, board, this.myFigure, lastMoves[i]);
+//					myLongestRow = Math.max(myLongestRow, this.longestRow(board, this.myFigure, lastMoves[i]));
 				else
-					longestEnemyRow = Math.max(longestEnemyRow,
-							this.longestRow(board, this.enemyFigure, lastMoves[i]));
+					longestEnemyRow = this.compareLongestRow(myLongestRow, board, this.enemyFigure, lastMoves[i]);
+//					longestEnemyRow = Math.max(longestEnemyRow, this.longestRow(board, this.enemyFigure, lastMoves[i]));
 			}
 		}
+		return this.getEvaluation(myLongestRow, longestEnemyRow, isMaximizing);
+//		if ((isMaximizing && (myLongestRow >= longestEnemyRow))
+//				|| (!isMaximizing && (myLongestRow > longestEnemyRow))) {
+//			this.maxValue = Math.max(myLongestRow, this.maxValue);
+//			return myLongestRow;
+//		} else {
+//			this.minValue = Math.min(-longestEnemyRow, this.minValue);
+//			return -longestEnemyRow;
+//		}
+
+//		if (isMaximizing) {
+//			if (myLongestRow >= longestEnemyRow) {
+//				this.maxValue = Math.max(myLongestRow, this.maxValue);
+//				return myLongestRow;
+//			} else {
+//				this.minValue = Math.min(-longestEnemyRow, this.minValue);
+//				return -longestEnemyRow;
+//			}
+//		} else {
+//			if (myLongestRow > longestEnemyRow) {
+//				this.maxValue = Math.max(myLongestRow, this.maxValue);
+//				return myLongestRow;
+//			} else {
+//				this.minValue = Math.min(-longestEnemyRow, this.minValue);
+//				return -longestEnemyRow;
+//			}
+//		}
+	}
+
+	// evaluatePoints and evaluateBoard could use the same evaluation for code
+	// reduction / simplification
+	/**
+	 * creates a hashmap containing all points and a simple evaluation of it
+	 *
+	 * @param board
+	 * @param allPoints all points that should be evaluated
+	 * @return evaluated hashmap
+	 */
+	public HashMap<Point, Integer> evaluatePoints(char[][] board, ArrayList<Point> allPoints) {
+		var evaluatedPoints = new HashMap<Point, Integer>();
+		for (var point : allPoints) {
+			if (this.winPossible(board, point)) {
+//				var evaluation = Math.max(this.longestRow(board, this.myFigure, point),
+//						this.longestRow(board, this.enemyFigure, point));
+				evaluatedPoints.put(point, this.getLongestRow(board, point));
+			}
+		}
+		return evaluatedPoints;
+	}
+
+	public int getEvaluation(final int myLongestRow, final int longestEnemyRow, final boolean isMaximizing) {
+//		if ((isMaximizing && (myLongestRow >= longestEnemyRow))
+//				|| (!isMaximizing && (myLongestRow > longestEnemyRow))) {
+//			this.maxValue = Math.max(myLongestRow, this.maxValue);
+//			return myLongestRow;
+//		} else {
+//			this.minValue = Math.min(-longestEnemyRow, this.minValue);
+//			return -longestEnemyRow;
+//		}
 		if (isMaximizing) {
 			if (myLongestRow >= longestEnemyRow) {
 				this.maxValue = Math.max(myLongestRow, this.maxValue);
@@ -51,39 +111,30 @@ public class BoardEvaluation {
 		}
 	}
 
-	// evaluatePoints and evaluateBoard could use the same evaluation for code
-	// reduction / simplification
-	/**
-	 * creates a hashmap containing all points and a simple evaluation of it
-	 *
-	 * @param board
-	 * @param allPoints all points that should be evaluated
-	 * @return evaluated hashmap
-	 */
-	public HashMap<Point, Integer> evaluatePoints(char[][] board, ArrayList<Point> allPoints) {
-		var evaluatedPoints = new HashMap<Point, Integer>();
-		for (var point : allPoints) {
-			if (this.winPossible(board, point)) {
-				var evaluation = Math.max(this.longestRow(board, this.myFigure, point),
-						this.longestRow(board, this.enemyFigure, point));
-				evaluatedPoints.put(point, evaluation);
-			}
-		}
-		return evaluatedPoints;
-	}
-
 	public boolean hasWon(char[][] board, Point lastMove) {
 		return Turn.hasWon(board, lastMove, board[lastMove.x][lastMove.y]);
 //		var figure = board[lastMove.x][lastMove.y];
 //		return (this.longestRow(board, figure, lastMove) > 4);
 	}
 
+	private int compareLongestRow(final int initialValue, final char[][] board, final char figure,
+			final Point coordinates) {
+		return Math.max(initialValue, this.longestRow(board, this.myFigure, coordinates));
+	}
+
+	private boolean fieldIsEmpty(final char[][] board, final Point coordinates) {
+		return board[coordinates.x][coordinates.y] == ' ';
+	}
+
+	private int getLongestRow(final char[][] board, final Point coordinates) {
+		return Math.max(this.longestRow(board, this.myFigure, coordinates),
+				this.longestRow(board, this.enemyFigure, coordinates));
+	}
+
 	private int longestPossibleRow(final char[][] board, final char figure, final char blank,
 			final Point coordinates) {
 		return new LongestRow(board, coordinates, figure).longestPossibleRow(blank);
 	}
-
-	// from class Turn
 
 	/**
 	 * returns longest row starting from given coordinates
@@ -94,6 +145,10 @@ public class BoardEvaluation {
 	 */
 	private int longestRow(final char[][] board, final char figure, final Point coordinates) {
 		return new LongestRow(board, coordinates, figure).run();
+	}
+
+	private boolean positionIsEven(final int position) {
+		return (position % 2) == 0;
 	}
 
 	private boolean winPossible(char[][] board, Point move) {
